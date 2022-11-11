8c623f34d15d2252a1aff5931394952b88440be0
Marcel Reig <marcelreig@gmail.com>
2022-11-11 18:31:37 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import ComponentA from "./components/ComponentA";
+import { Contact } from "./models/contact.class";
 
 function App() {
+  const [newContact, setContact] = useState(
+    new Contact("Marcel", "Reig", "marcel@ibm.com", false)
+  );
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ComponentA contact={newContact} setContact={setContact}></ComponentA>
       </header>
     </div>
   );
